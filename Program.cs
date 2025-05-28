66d6717640007534023e36a4c9a18df33b4d0475
Jakub Kwaśniewski <jf246962@student.polsl.pl>
2025-05-28 19:10:29 +0200

    done

--- Program.cs
+++ Program.cs
@@ -4,7 +4,33 @@ class Program
     static void Main(string[] args)
     {
 
-        //Zadania będą na oddzielnych branchach
+        double highestResult = 0;
+        double sum = 0;
+        Console.WriteLine("Podaj wartości liczbowe: (0 kończy program).");
+        while (true)
+        {
+            if (double.TryParse(Console.ReadLine(), out double res))
+            {
+                if (res == 0.0)
+                {
+                    Console.WriteLine("Suma: " + sum);
+                    Console.WriteLine("Największa liczba: " + highestResult);
+                    break;
+                }
+                else
+                {
+                    sum += res;
+                    if (res > highestResult) highestResult = res;
+                }
+
+            }
+
+            else
+            {
+                Console.WriteLine("Nie podałeś liczby, przerywam program");
+                break;
+            }
+        }
     }
 }
 
