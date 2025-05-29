8972c6075cc76048a9547e59f7ef3afcd203e4b0
Jakub Kwaśniewski <jf246962@student.polsl.pl>
2025-05-29 22:19:29 +0200

    done

--- Program.cs
+++ Program.cs
@@ -1,6 +1,26 @@
 ﻿
 class Program
 {
+
+    private List<string> tasks = new List<string>();
+
+    public void AddTask(string task)
+    {
+        // TODO: Uzupełnij implementację metody AddTask, aby dodawała nowe zadanie do listy.
+        tasks.Add(task);
+    }
+
+    public void RemoveTask(string task)
+    {
+        // TODO: Uzupełnij implementację metody RemoveTask, aby usuwała zadanie z listy, jeśli istnieje.
+        tasks.Remove(task);
+    }
+
+    public List<string> GetTasks()
+    {
+        // TODO: Uzupełnij implementację metody GetTasks, aby zwracała listę wszystkich zadań.
+        return tasks;
+    }
     static void Main(string[] args)
     {
 
