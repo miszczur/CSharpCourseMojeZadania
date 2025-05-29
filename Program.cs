7d74f9d19454512a94bc04d23ce4674b91e40bb5
Jakub Kwaśniewski <jf246962@student.polsl.pl>
2025-05-29 17:35:46 +0200

    done

--- Program.cs
+++ Program.cs
@@ -2,9 +2,16 @@
 class Program
 {
     static void Main(string[] args)
-    {
+    { 
 
-        //Zadania będą na oddzielnych branchach
+        DateTime currentDate = DateTime.Now;        
+
+        Console.WriteLine("Podaj datę swoich urodzin w formacie dd.mm.yyyy:");
+        string userInput = Console.ReadLine();
+
+        DateTime userDateTime = DateTime.Parse(userInput);
+        TimeSpan ts = currentDate - userDateTime;
+        Console.WriteLine($"Minęło: {ts.TotalDays} dni");
     }
 }
 
