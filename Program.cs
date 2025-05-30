d16e0f26874c918d2e9ad1a7b3ec7e1334d81eee
Jakub Kwaśniewski <jf246962@student.polsl.pl>
2025-05-30 19:56:07 +0200

    done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
-﻿
+﻿class EvenNumberGenerator
+{
+    public IEnumerable<int> GenerateEvenNumbers()
+    {
+        for (int i = 0; i < int.MaxValue; i++)
+        {
+            if (i % 2 == 0) yield return i;
+        }
+    }
+}
 class Program
 {
     static void Main(string[] args)
@@ -9,3 +18,13 @@ class Program
 }
 
 
+/*
+ * Opis: Twoim zadaniem jest napisanie programu, który będzie generował kolejne liczby parzyste.
+ * Użyj konstrukcji yield do generowania tych liczb.
+
+Wymagania:
+
+Zaimplemenuj klasę EvenNumberGenerator zawierającą metodę GenerateEvenNumbers, która będzie generować kolejne liczby parzyste.
+
+Użyj konstrukcji yield w metodzie GenerateEvenNumbers do generowania liczb parzystych.
+*/
