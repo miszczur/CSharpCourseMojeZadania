f031c7a7d2cb83f3fc7f5b3234ed41c20b275304
Jakub Kwaśniewski <jf246962@student.polsl.pl>
2025-05-31 14:59:44 +0200

    done

--- Program.cs
+++ Program.cs
@@ -1,11 +1,37 @@
 ﻿
+
+public class TemperatureConverter
+{
+    public static void ConvertCelsiusToFahrenheit(double celsius, out double fahrenheit)
+    {
+       fahrenheit = celsius * 1.8 + 32;
+    }
+
+    public static void ConvertFahrenheitToCelsius(double fahrenheit, ref double celsius)
+    {
+        celsius = (fahrenheit - 32) / 1.8;
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
 
-        //Zadania będą na oddzielnych branchach
+        double dd = 20;
+        Console.WriteLine(dd);
+        TemperatureConverter.ConvertFahrenheitToCelsius(100, ref dd);
+        Console.WriteLine(dd);
     }
 }
 
 
+/*ref i out
+Zadanie praktyczne: Konwersja temperatury
+
+Opis: Twoim zadaniem jest napisanie programu, który będzie konwertować temperaturę z jednej skali (np. Celsiusza) na inną skalę (np. Fahrenheit). Użyj parametrów ref i out do przekazywania wyników z funkcji.
+
+Wymagania:
+
+Zaimplementuj klasę TemperatureConverter zawierającą dwie metody: ConvertCelsiusToFahrenheit i ConvertFahrenheitToCelsius.
+
+Wykorzystaj formuły konwersji odpowiednie dla każdej skali.*/
