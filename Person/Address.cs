32f6b428588dc4ff3b5a71c2b3261b8280839e80
Jakub Kwaśniewski <jfmysc2@gmail.com>
2025-06-02 12:05:25 +0200

    done

---
+++ Person/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpCourseMojeZadania
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,79 @@
-﻿
-class Program
+﻿using Newtonsoft.Json;
+namespace CSharpCourseMojeZadania
 {
-    static void Main(string[] args)
+    class Program
     {
+        static void Main(string[] args)
+        {
+            var people = LoadPeople();
+            var addresses = LoadAddresses();
 
-        //Zadania będą na oddzielnych branchach
+            //JOIN
+            //         var data = people.Join()
+
+            //var nullAddr = addresses.FirstOrDefault(a => a.Id != a.PersonId);
+            //var joinedData = people.Join(addresses,
+            //    p => p.Id,
+            //    a => a.PersonId,
+            //    (person, address) => new { person.Name, address.City  });
+
+            //foreach (var person in joinedData)
+            //{
+            //    Console.WriteLine($"Person Name: {person.Name}, City: {person.City}");
+            //}
+
+            //GROUPJOIN
+            //var groupJoinedData = people.GroupJoin(addresses,
+            //    p => p.Id,
+            //    a => a.PersonId,
+            //    (person, addresses) => new { person.Name, Addresses = addresses });
+
+            //foreach (var element in groupJoinedData)
+            //{
+            //    Console.WriteLine($"Name : {element.Name}");
+            //    foreach (var address in element.Addresses)
+            //    {
+            //        Console.WriteLine($"\t City: {address.City}, street: {address.Street}");
+            //    }
+            //}
+
+            var joinedData = people.GroupJoin(
+                addresses,
+                person => person.Id,
+                address => address.PersonId,
+                (person, addressGroup) => new { person, addressGroup })
+                .SelectMany(
+                    temp => temp.addressGroup.DefaultIfEmpty(), // zapewnia LEFT JOIN
+                    (temp, address) => new
+                    {
+                        Name = temp.person.Name,
+                        City = address?.City ?? "Brak adresu" // obsługa nulli
+                    });
+
+            foreach (var person in joinedData)
+            {
+                Console.WriteLine($"Person Name: {person.Name}, City: {person.City}");
+            }
+            Console.WriteLine();
+        }
+
+        private static List<Person> LoadPeople()
+        {
+            var currentDir = @"C:\Users\jfmys\source\repos\CSharpCourseMojeZadania\Person\Person.json";
+            var jsonFullPath = @"C:\Users\jfmys\source\repos\CSharpCourseMojeZadania\Person\People.json";
+
+            var json = File.ReadAllText(jsonFullPath);
+
+            return JsonConvert.DeserializeObject<List<Person>>(json);
+        }
+        private static List<Address> LoadAddresses()
+        {
+            var currentDir = @"C:\Users\jfmys\source\repos\CSharpCourseMojeZadania";
+            var jsonFullPath = @"C:\Users\jfmys\source\repos\CSharpCourseMojeZadania\Person\Addresses.json";
+
+            var json = File.ReadAllText(jsonFullPath);
+
+            return JsonConvert.DeserializeObject<List<Address>>(json);
+        }
     }
 }
-
-
